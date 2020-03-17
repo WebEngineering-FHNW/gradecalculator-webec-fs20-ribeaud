298a32c3709ed260047363efa373a1857c4bc3aa
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2020-03-17 15:54:21 +0100

    Add declarative validation

--- grails-app/controllers/webec/CalculatorController.groovy
+++ grails-app/controllers/webec/CalculatorController.groovy
@@ -3,13 +3,15 @@ package webec
 class CalculatorController {
 
     def calc(CalculatorModel calculatorModel) {
-        if (calculatorModel.en < 1) {
-            calculatorModel.result = "Can NOT calculate. En was invalid."
-        } else if (calculatorModel.msp < 1) {
-            calculatorModel.result = "Can NOT calculate. Msp was invalid."
-        } else {
+        if (calculatorModel.validate()) {
             double result = (calculatorModel.en + calculatorModel.msp) / 2
             calculatorModel.result = result
+        } else {
+            if (calculatorModel.errors.fieldErrors.any { it.field == 'en' }) {
+                calculatorModel.result = "Can NOT calculate. En was invalid."
+            } else if (calculatorModel.errors.fieldErrors.any { it.field == 'msp' }) {
+                calculatorModel.result = "Can NOT calculate. Msp was invalid."
+            }
         }
         render view: 'CalculatorOutput', model: [calculatorModel: calculatorModel]
     }
@@ -19,4 +21,10 @@ class CalculatorModel {
     double en
     double msp
     String result = ''
+
+    static constraints = {
+        en min: 1d, max: 6d, scale: 1
+        msp min: 1d, max: 6d, scale: 1
+        result nullable: null
+    }
 }
